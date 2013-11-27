e872978118c44c72aeb2ada84cf220c5e8fc0567
Flávio Zavan <flaviozavan@gmail.com>
2013-11-27 11:51:39 -0200

    Não incluir css desnecessário

--- includes/Contribua.php
+++ includes/Contribua.php
@@ -159,8 +159,6 @@ class Contribua
 
 	public static function addStylesheet()
 	{
-		wp_enqueue_style('style-jqueryui',
-				'http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css');
 		wp_enqueue_style('style-widget',
 				plugins_url('/contribua/assets/css/widget.css', CONTRIBUA_PATH));
 		wp_enqueue_style('style-page',
