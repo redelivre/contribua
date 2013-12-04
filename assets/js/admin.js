a861a392a538f00b9a239762c205bcf4549d82ac
Flávio Zavan <flaviozavan@gmail.com>
2013-12-04 13:48:24 -0200

    Corrige efeito no painel

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,3 +1,17 @@
 jQuery( document ).ready(function(){
 	jQuery('.contribua-color-picker').wpColorPicker();
+
+	jQuery('input:checkbox[name^=contribuicao]').on('click', function()
+	{
+		var contentElement = jQuery(this).nextAll('div:first');
+
+		if (!contentElement.is(':visible'))
+		{
+			contentElement.fadeIn('fast');
+		}
+		else
+		{
+			contentElement.fadeOut('fast');
+		}
+	});
 });
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,4 @@
 jQuery( document ).ready(function(){
-	jQuery('input:checkbox[name^=contribuicao]').on('click', function(){
-		var contentElement = jQuery(this).parent().next('div');
-
-		if (!contentElement.is(':visible')) {
-			contentElement.fadeIn('fast');
-		}
-		else {
-			contentElement.fadeOut('fast');
-		}
-	});
-
 	jQuery('.contribua-price').keyup(function (event)
 	{
 		contribuaPriceFormat(this);
